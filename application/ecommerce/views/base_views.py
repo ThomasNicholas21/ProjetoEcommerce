ef00e95760f089a4f2c07e603c201427b621a40d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-21 12:56:30 -0300

    feat: criando view para about

--- application/ecommerce/views/base_views.py
+++ application/ecommerce/views/base_views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ecommerce/index.html')
+
+def about(request):
+    return render(request, 'ecommerce/about.html')
