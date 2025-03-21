2443e789a977520382b5e1967d831c50c7cc893e
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-21 12:55:48 -0300

    feat: criando url para about

--- application/ecommerce/urls.py
+++ application/ecommerce/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from ecommerce.views import index
+from ecommerce.views import index, about
 
 app_name='ecommerce'
 
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('about/', about, name='about'),
 ]
