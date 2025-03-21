9fc0b341f333fb241fc20f299dabf2521182bd15
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-21 10:42:30 -0300

    feat: implementando SiteConfig e configurando no admin

--- application/site_config/admin.py
+++ application/site_config/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from site_config import models
 
 # Register your models here.
+
+@admin.register(models.SiteConfig)
+class SiteConfigAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'contact_number', 
+        'contact_email', 
+        )
+    list_display_links = 'name',
--- application/site_config/models.py
+++ application/site_config/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class SiteConfig(models.Model):
+    class Meta:
+        verbose_name = "Configuration"
+        verbose_name_plural = "Configurations"
+
+    name = models.CharField(max_length=65, default='E-commerce | Change-me')
+    address = models.TextField()
+    contact_number = models.CharField(max_length=35)
+    contact_email = models.EmailField(max_length=254)
+    about = models.TextField()
+    cnpj = models.CharField(max_length=18, unique=True, blank=True, null=True)
+    cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)
+    logo = models.ImageField(upload_to="logo/Y%/%m", blank=True, default='')
+    favicon = models.ImageField(upload_to="assets/favicon/%Y/%m", blank=True, default='') # inserir validaçöes de imagem futuramente
+
+    def __str__(self):
+        return self.name
+    
