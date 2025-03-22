15e4767fc13d11b0e8e4f52e18d6ecfcbd5f879e
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-22 19:52:08 -0300

    feat: salvando slug e validando imagens do model product

--- application/ecommerce/models/product_models.py
+++ application/ecommerce/models/product_models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from utils.slug.slug_gen import new_slug
+from utils.images.image_validator import resize_image
+from utils.validators.validate_image import is_png_svg
 
 class Product(models.Model):
     PRODUCTS_TYPE = (
@@ -12,7 +14,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=65)
     slug = models.SlugField(
-        max_length=255, default=None,
+        max_length=255,
         null=True, blank=True,
         unique=True,
         )
@@ -20,14 +22,35 @@ class Product(models.Model):
     long_description = models.TextField()
     product_image = models.ImageField(
         upload_to="products/%Y/%m", 
-        blank=True, default=""
+        blank=True, default="",
+        validators=[is_png_svg,]
         )
-    price = models.FloatField()
-    promotional_price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    promotional_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     product_type = models.CharField(
-        default=None, max_length=1, choices=PRODUCTS_TYPE
+        default=None, max_length=1, choices=PRODUCTS_TYPE,
+        help_text=(
+            'Produto "Simples" não possui variações, sendo ele único. '
+            'Já o produto "Variável" é um produto cujo possui variações, por exemplo, cores e tamanhos diferentes.'
+            )
     )
-    stock = models.IntegerField(default=0)
+    stock = models.IntegerField(blank=False, default=0, help_text="Coloque quantos produtos tem em estoque.")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slug(self.name, 5)
+
+        current_pruduct_image_name = str(self.product_image.name)
+        super_save = super().save(*args, **kwargs)
+        product_image_changed = False
+
+        if self.product_image:
+            product_image_changed = current_pruduct_image_name != self.product_image.name
+        
+        if product_image_changed:
+            resize_image(self.product_image)
+
+        return super_save
 
     def __str__(self):
         return self.name
