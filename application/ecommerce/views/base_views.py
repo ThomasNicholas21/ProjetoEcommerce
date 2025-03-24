55393a02c74471ac9e5d39b40eb3fff454ab7727
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-24 17:49:37 -0300

    chore: retirando módulos inutulizados

--- application/ecommerce/views/base_views.py
+++ application/ecommerce/views/base_views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from ecommerce.models import Product
 from django.views.generic import ListView
 from django.shortcuts import redirect
