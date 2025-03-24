2490c07363efdb1c24bcaf243dac7f025ecd808f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-24 10:26:42 -0300

    feat: implementando paginação para página home

--- application/ecommerce/views/base_views.py
+++ application/ecommerce/views/base_views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from ecommerce.models import Product
 # Create your views here.
 
+PER_PAGE = 6
 
 def index(request):
-    return render(request, 'ecommerce/index.html')
+    products = Product.objects.order_by('-id').filter(active=True).all()
+
+    paginator = Paginator(products, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj
+    }
+
+    return render(request, 'ecommerce/index.html', context)
 
 def about(request):
     return render(request, 'ecommerce/about.html')
