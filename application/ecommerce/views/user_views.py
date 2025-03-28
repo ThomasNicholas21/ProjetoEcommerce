40ebae7a02185ff67399828583ed0da78e8c7000
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-28 11:13:28 -0300

    feat: criando view utilizando formview para o cadastro do usuário

--- application/ecommerce/views/user_views.py
+++ application/ecommerce/views/user_views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from ecommerce.forms import RegisterForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 def user_register(request):
     return render(request, 'ecommerce/user/register.html') 
 
+class UserRegisterFormView(FormView):
+    template_name = 'ecommerce/user/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('ecommerce:index')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
