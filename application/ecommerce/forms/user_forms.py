96c2834ab7c623713a2535ec3ddd6162d3cff52b
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-28 17:54:39 -0300

    feat: implementando forms de update para user

--- application/ecommerce/forms/user_forms.py
+++ application/ecommerce/forms/user_forms.py
@@ -135,3 +135,133 @@ class RegisterForm(UserCreationForm):
             self.add_error('email', msg_error)
 
         return email
+
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name',
+            'email', 'username',
+            'password1', 'password2',
+        )
+
+    username = forms.CharField(
+        label='Usuário',
+        required=True,
+        min_length=2,
+        max_length=40,
+        help_text=mark_safe(
+                "Use apenas caracteres alfanuméricos e os seguintes símbolos: @ . + - _<br>"
+                "Exemplos válidos: nome.sobre+nome@exemplo.com, usuario_123"
+        )
+    )
+
+    first_name = forms.CharField(
+        label='Primeiro Nome',
+        required=True,
+        min_length=2,
+        max_length=40,
+    )
+
+    last_name = forms.CharField(
+        label='Sobrenome',
+        required=True,
+        min_length=2,
+        max_length=40,
+    )
+
+    email = forms.EmailField(
+        label='E-mail',
+        required=True,
+    )
+
+    password1 = forms.CharField(
+        label='Senha',
+        strip=False,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+            }
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label='Confirme a senha',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+            }
+        ),
+        strip=False,
+        help_text=('Coloque a mesma senha de antes, para verificação.'),
+    )
+
+    def save(self, commit=True):
+        data = self.cleaned_data
+        user = super().save(commit=False)
+        password = data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+    
+    def clean(self):
+        data = super().clean()
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+
+        if first_name == last_name:
+            msg_first = ValidationError(
+                'Primeiro nome deve ser diferente do sobrenome.',
+                code='invalid'
+            )
+            msg_second = ValidationError(
+                'Sobrenome deve ser diferent do primeiro nome.',
+                code='invalid'
+            )
+        
+            self.add_error('first_name', msg_first)
+            self.add_error('last_name', msg_second)
+
+        return data
+
+    def clean_password(self):
+        data = self.cleaned_data
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+
+        if password1 != password2:
+            msg_first = ValidationError(
+                'Primeira senha deve ser igual a segunda',
+                code='invalid'
+            )
+            self.add_error('password1', msg_first)
+        
+        if password2 != password1:
+            msg_second = ValidationError(
+                'Essa senha não corresponde a primeira.',
+                code='invalid'
+            )
+            self.add_error('password2', msg_second)
+
+        return super().clean()
+    
+    def clean_email(self):
+        data = self.cleaned_data
+        email = data.get('email')
+
+        if User.objects.filter(email=email):
+            msg_error = ValidationError(
+                'Já existe uma conta com esse e-mail.',
+                code='Invalid'
+            )
+            self.add_error('email', msg_error)
+
+        return email
