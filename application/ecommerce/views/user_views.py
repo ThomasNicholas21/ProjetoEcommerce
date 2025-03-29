e9057f4d1e624f3ec2c513959c5452ce0380d3a2
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-03-29 13:37:22 -0300

    feat: implementando placeholder dos dados do usuário ao atualiza-lo

--- application/ecommerce/views/user_views.py
+++ application/ecommerce/views/user_views.py
@@ -22,8 +22,13 @@ class UserUpdateFormView(FormView):
     form_class = UpdateUserForm
     success_url = reverse_lazy('ecommerce:update_user')
 
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        return form_class(instance=self.request.user, **self.get_form_kwargs())
+
     def form_valid(self, form):
-        form = UpdateUserForm(data=self.request.POST, instance=self.request.user)
+        form.save()
         return super().form_valid(form)
     
 
