e21001bccf81676433dda5e739f6d28c7606affd
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-02 23:55:55 -0300

    feat: criando tabularinline para order e seus itens

--- application/ecommerce/admin/order_admin.py
+++ application/ecommerce/admin/order_admin.py
@@ -3,11 +3,11 @@ from ecommerce.models.order_models import Order, OrderItem
 # Register your models here.
 
 
+class OrderItemTabularInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
 @admin.register(Order)
 class UserProfileAdmin(admin.ModelAdmin):
-    ...
-
-
-@admin.register(OrderItem)
-class UserProfileAdmin(admin.ModelAdmin):
-    ...
+    inlines = [OrderItemTabularInline]
