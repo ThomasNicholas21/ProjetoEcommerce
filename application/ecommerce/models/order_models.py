59b43e911e32455843e15070d96bc8a56e6322df
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-05-06 17:56:51 -0300

    chore: removendo função inutilizada

--- application/ecommerce/models/order_models.py
+++ application/ecommerce/models/order_models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from utils.slug.slug_gen import new_slug
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
